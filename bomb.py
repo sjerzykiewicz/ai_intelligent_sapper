060beec3eb23423ca20b0bb33415d0dae334de33
yanvoi <janwojciechowski@op.pl>
2023-03-29 13:28:47 +0200

    I've reformatted our codebase with the black formatter

--- bomb.py
+++ bomb.py
@@ -1,4 +1,4 @@
-class Bomb():
+class Bomb:
     def __init__(self, position, image_path, defusable, weight):
         self.position = position
         self.image_path = image_path
--- surface.py
+++ surface.py
@@ -1,6 +1,6 @@
-class Surface():
+class Surface:
     def __init__(self, position, image_path, type, slowing_power):
         self.position = position
         self.image_path = image_path
         self.type = type
-        self.slowing_power = slowing_power 
+        self.slowing_power = slowing_power
