3341d12a3acb3a0eb9cb359e88eec98f53055c5a
s_473553 <janwojciechowski@op.pl>
2023-05-27 21:28:13 +0200

    changed action to answer

--- decision_tree/generate_attribiutes.py
+++ decision_tree/generate_attribiutes.py
@@ -9,40 +9,40 @@ posibilities = {
     "is_barrel_nearby": ["yes", "no"],
     "sapper_type": ["standard", "rain_defusing"],
     "is_low_temp": ["yes", "no"],
-    "action": ["defuse"],
+    "answer": ["defuse"],
 }
 
 keys, values = zip(*posibilities.items())
 perms = [dict(zip(keys, v)) for v in product(*values)]
 for perm in perms:
     if perm["surface_type"] == "sand":
-        perm["action"] = "defuse"
+        perm["answer"] = "defuse"
     if perm["surface_type"] == "unpaved_road":
-        perm["action"] = "move"
+        perm["answer"] = "move"
     if perm["bomb_type"] == "claymore":
-        perm["action"] = "defuse"
+        perm["answer"] = "defuse"
     if perm["dist_from_flag"] == "<=10":
-        perm["action"] = "move"
+        perm["answer"] = "move"
     if perm["weather"] == "rainy":
-        perm["action"] = "move"
+        perm["answer"] = "move"
     if perm["time_of_day"] == "night":
-        perm["action"] = "defuse"
+        perm["answer"] = "defuse"
     if perm["is_barrel_nearby"] == "yes":
         if perm["is_low_temp"] == "yes":
-            perm["action"] = "defuse"
+            perm["answer"] = "defuse"
         else:
-            perm["action"] = "move"
+            perm["answer"] = "move"
     if perm["sapper_type"] == "rain_defusing":
         if perm["weather"] == "rainy":
-            perm["action"] = "defuse"
+            perm["answer"] = "defuse"
     if perm["bomb_type"] == "hcb":
-        perm["action"] = "move"
+        perm["answer"] = "move"
 
 with open("input.csv", "w") as f:
     f.write(
-        "dist_from_flag,bomb_type,surface_type,weather,time_of_day,is_barrel_nearby,sapper_type,is_low_temp,action\n"
+        "dist_from_flag,bomb_type,surface_type,weather,time_of_day,is_barrel_nearby,sapper_type,is_low_temp,answer\n"
     )
     for perm in perms:
         f.write(
-            f'{perm["dist_from_flag"]},{perm["bomb_type"]},{perm["surface_type"]},{perm["weather"]},{perm["time_of_day"]},{perm["is_barrel_nearby"]},{perm["sapper_type"]},{perm["is_low_temp"]},{perm["action"]}\n'
+            f'{perm["dist_from_flag"]},{perm["bomb_type"]},{perm["surface_type"]},{perm["weather"]},{perm["time_of_day"]},{perm["is_barrel_nearby"]},{perm["sapper_type"]},{perm["is_low_temp"]},{perm["answer"]}\n'
         )
