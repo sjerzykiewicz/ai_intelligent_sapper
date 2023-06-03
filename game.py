7b40f0f1191bbc437ded2e66aafa02b9c530e509
s_473553 <janwojciechowski@op.pl>
2023-06-03 13:25:37 +0200

    sapper now goes from place to place searching for bombs

--- game.py
+++ game.py
@@ -63,10 +63,6 @@ class Game:
         self.sapper_type = self._get_sapper_type()
         self.weather, self.time = self._get_weather_and_time()
         self.is_low_temperature = choice(["yes", "no"])
-        if self.is_low_temperature == "yes":
-            print("Low temperature!")
-        else:
-            print("Normal temperature!")
 
         self.sapper = StandardSapper(
             place_for_sapper,
